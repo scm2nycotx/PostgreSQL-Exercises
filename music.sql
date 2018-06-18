51dffc0641ec659c06881bf9815467d14fbdf6ee
Chih-Ming Sun <scm2nycotx@gmail.com>
2018-06-18 08:26:21 +0000

    music_queries.sql added

--- music.sql
+++ music.sql
@@ -1,32 +1,120 @@
+CREATE TABLE artist (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR UNIQUE NOT NULL
+  );
+  
+CREATE TABLE song_writer (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR UNIQUE NOT NULL
+  );
+
+CREATE TABLE song (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR UNIQUE NOT NULL,
+  song_writer_id INTEGER REFERENCES song_writer (id)
+  );
+
 CREATE TABLE album (
   id SERIAL PRIMARY KEY,
   title VARCHAR UNIQUE NOT NULL,
-  artist VARCHAR,
   release date,
-  leading_artist_id INTEGER REFERENCES artist (id)
-  label VARCHAR
+  artist_id INTEGER REFERENCES artist (id)
   );
 
 CREATE TABLE track (
   id SERIAL PRIMARY KEY,
   album_id INTEGER REFERENCES album (id),
   song_id INTEGER REFERENCES song (id),
-  song VARCHAR,
-  duration time,
+  duration TIME
   );
 
-CREATE TABLE artist (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR UNIQUE NOT NULL,
-  );
-  
-CREATE TABLE song (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR UNIQUE NOT NULL,
-  song_writer_id INTEGER REFERENCES song_writer (id),
-  );
+INSERT INTO artist VALUES (DEFAULT, 'DRAKE'),
+(DEFAULT, 'ED SHEERAN'),
+(DEFAULT, 'KENDRICK LAMAR'),
+(DEFAULT, 'BRUNO MARS'),
+(DEFAULT, 'TAYLOR SWIFT')
 
-CREATE TABLE song_writer (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR UNIQUE NOT NULL,
-  );
+INSERT INTO song_writer VALUES (DEFAULT, 'ANDY LIU'),
+(DEFAULT, 'JACKY WU'),
+(DEFAULT, 'LEON LI'),
+(DEFAULT, 'AARON KUAO'),
+(DEFAULT, 'CHIHMING SUN')
+
+INSERT INTO album VALUES (DEFAULT, 'Scorpion', '2018-05-26', 1),
+(DEFAULT, 'Views', '2016-02-03', 1),
+(DEFAULT, 'Nothing Was the Same', '2013-04-07', 1),
+(DEFAULT, '÷', '2017-11-12', 2),
+(DEFAULT, 'x', '2014-05-05', 2),
+(DEFAULT, '+', '2011-08-15', 2),
+(DEFAULT, 'Damn', '2017-04-01', 3),
+(DEFAULT, 'To Pimp a Butterfly', '2015-05-05', 3),
+(DEFAULT, 'Good Kid, M.A.A.D City', '2012-03-15', 3),
+(DEFAULT, '24K Magic', '2016-08-09', 4),
+(DEFAULT, 'Unorthodox Jukebox', '2012-02-20', 4),
+(DEFAULT, 'Doo-Wops & Hooligans', '2010-06-18', 4),
+(DEFAULT, 'Reputation', '2017-01-12', 5),
+(DEFAULT, '1989', '2014-10-11', 5),
+(DEFAULT, 'Red', '2012-09-09', 5)
+
+INSERT INTO song VALUES (DEFAULT, 'Nice for What', 1),
+(DEFAULT, 'God"s plan', 2),
+(DEFAULT, 'Keep the Family Close', 3),
+(DEFAULT, 'g', 4),
+(DEFAULT, 'Tuscan Leather', 5),
+(DEFAULT, 'Furthest Thing', 5),
+(DEFAULT, 'Eraser', 4),
+(DEFAULT, 'Castle on the Hill', 3),
+(DEFAULT, 'one', 2),
+(DEFAULT, 'I am a Mess', 1),
+(DEFAULT, 'The A Team', 1),
+(DEFAULT, 'Drunk', 2),
+(DEFAULT, 'Blood', 3),
+(DEFAULT, 'DNA', 4),
+(DEFAULT, 'Wesley"s Theory', 5),
+(DEFAULT, 'For Free?', 5),
+(DEFAULT, 'Sherane a.k.a Master Splinter"s Daughter', 4),
+(DEFAULT, 'Bitch, Don"t Kill My Vibe', 3),
+(DEFAULT, '24K Magic', 2),
+(DEFAULT, 'Chunky', 1),
+(DEFAULT, 'Young Girls', 1),
+(DEFAULT, 'Locked Out of Heaven', 2),
+(DEFAULT, 'Grenade', 3),
+(DEFAULT, 'Just the Way You Are', 4),
+(DEFAULT, '...Ready for It?', 5),
+(DEFAULT, 'End Game', 5),
+(DEFAULT, 'Welcome to New York', 4),
+(DEFAULT, 'Blank Space', 3),
+(DEFAULT, 'State of Grace', 2),
+(DEFAULT, 'Red', 1)
+
+INSERT INTO track VALUES (DEFAULT, 1, 1, '00:05:21'),
+(DEFAULT, 1, 2, '00:04:11'),
+(DEFAULT, 2, 3, '00:03:32'),
+(DEFAULT, 2, 4, '00:04:22'),
+(DEFAULT, 3, 5, '00:04:33'),
+(DEFAULT, 3, 6, '00:05:16'),
+(DEFAULT, 4, 7, '00:04:21'),
+(DEFAULT, 4, 8, '00:04:22'),
+(DEFAULT, 5, 9, '00:03:44'),
+(DEFAULT, 5, 10, '00:05:11'),
+(DEFAULT, 6, 11, '00:04:03'),
+(DEFAULT, 6, 12, '00:03:59'),
+(DEFAULT, 7, 13, '00:04:11'),
+(DEFAULT, 7, 14, '00:03:55'),
+(DEFAULT, 8, 15, '00:05:11'),
+(DEFAULT, 8, 16, '00:04:09'),
+(DEFAULT, 9, 17, '00:04:44'),
+(DEFAULT, 9, 18, '00:05:10'),
+(DEFAULT, 10, 19, '00:05:00'),
+(DEFAULT, 10, 20, '00:04:27'),
+(DEFAULT, 11, 21, '00:04:11'),
+(DEFAULT, 11, 22, '00:02:22'),
+(DEFAULT, 12, 23, '00:02:58'),
+(DEFAULT, 12, 24, '00:04:34'),
+(DEFAULT, 13, 25, '00:04:33'),
+(DEFAULT, 13, 26, '00:04:12'),
+(DEFAULT, 14, 27, '00:03:22'),
+(DEFAULT, 14, 28, '00:04:11'),
+(DEFAULT, 15, 29, '00:03:33'),
+(DEFAULT, 15, 30, '00:03:16')
+
---
+++ music_queries.sql
@@ -0,0 +1,57 @@
+--1. What are tracks for a given album?
+SELECT track.id AS track_id, album.title AS album_title, song.name AS song_name, album.id AS album_id FROM track INNER JOIN album ON track.album_id = album.id INNER JOIN song ON track.song_id = song.id WHERE album.id = 9
+--2. What are the albums produced by a given artist?
+SELECT album.id AS album_id, album.title AS album_title, artist.id AS artist_id, artist.name AS artist_name FROM album INNER JOIN artist ON album.artist_id = artist.id WHERE artist.id = 5
+--3. What is the track with the longest duration?
+SELECT track.id AS track_id, album.title AS album_title, song.name AS song_name, track.duration AS max_duration FROM track INNER JOIN album ON track.album_id = album.id INNER JOIN song ON track.song_id = song.id WHERE duration = (SELECT MAX(duration) FROM track) 
+--4. What are the albums released in the 60s? 70s? 80s? 90s? 2015~2018?
+SELECT album.title, album.release FROM album WHERE album.release < '1970-01-01' and album.release > '1959-12-31' 
+SELECT album.title, album.release FROM album WHERE album.release < '1980-01-01' and album.release > '1969-12-31' 
+SELECT album.title, album.release FROM album WHERE album.release < '1990-01-01' and album.release > '1979-12-31' 
+SELECT album.title, album.release FROM album WHERE album.release < '2000-01-01' and album.release > '1989-12-31'
+SELECT album.title, album.release FROM album WHERE album.release < '2019-01-01' and album.release > '2014-12-31' 
+--5. How many albums did a given artist produce in the 90s? 2015~2018?
+SELECT album.title, album.release FROM album INNER JOIN artist ON album.artist_id = artist.id WHERE artist.id = 2 and album.release < '2000-01-01' and album.release > '1989-12-31'
+SELECT album.title, album.release FROM album INNER JOIN artist ON album.artist_id = artist.id WHERE artist.id = 2 and album.release < '2019-01-01' and album.release > '2014-12-31'
+--6. What is each artist's latest album?
+SELECT artist.id, artist.name, album.title, album.release AS latest_release FROM artist INNER JOIN album on artist.id = album.artist_id WHERE release IN (SELECT MAX(release) FROM album GROUP BY artist_id)
+--7. List all albums along with its total duration based on summing the duration of its tracks.
+SELECT album.id AS album_id, album.title, SUM(track.duration) AS total_duration FROM album INNER JOIN track on album.id = track.album_id GROUP BY album.id ORDER BY album.id
+--8. What is the album with the longest duration?
+-->SELECT track.album_id, MAX(track.total_duration) AS max_total_duration FROM (SELECT SUM(track.duration) AS total_duration FROM track GROUP BY album_id)
+-->ERROR: pq: subquery in FROM must have an alias
+--9. Who are the 5 most prolific artists based on the number of albums they have recorded?
+
+--10. What are all the tracks a given artist has recorded?
+
+--11. What are the top 5 most often recorded songs?
+
+--12. Who are the top 5 song writers whose songs have been most often recorded?
+
+--13. Who is the most prolific song writer based on the number of songs he has written?
+
+--14. What songs has a given artist recorded?
+
+--15. Who are the song writers whose songs a given artist has recorded?
+
+--16. Who are the artists who have recorded a given song writer's songs?
+
+--Bonus Challenge 1
+--Allow that an album may have multiple artists:
+-->> a lead artist
+-->> any number of collaborators
+--Given a lead artist, what collaborators has he worked with? Hint: you can give the same table 2 different aliases. For example, a query to find all people you follow would look like `select from "user" as follower, "user" as followee where ...
+
+--Super challenge: given an artist who has worked as a collaborator, who are the other collaborators he has worked with?
+
+--Bonus Challenge 2
+--Willie Nelson wishes to be able to search for music by musical instrument. For example:
+-->> Get the list of tracks with a violin in it. (You can sub in your instrument of choice)
+-->> Get the list of tracks with both a harmonica and an accordion. (Again, sub in your instruments of choice)
+-->> Get the list of vocal tracks.
+-->> Get the list of instrumental tracks (no vocal).
+-->> Get a list of piano solo tracks (piano and no other instrument).
+--Make his wish come true.
+
+
+
